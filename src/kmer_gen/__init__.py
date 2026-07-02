5b97d3f9a1ec26d4bffaa3daa1afa7c4e2290696
Aran Garrod <aran.garrod@gmail.com>
2026-07-02 12:14:54 +0100

    Updating imports

--- src/kmer_gen/__init__.py
+++ src/kmer_gen/__init__.py
@@ -2,10 +2,12 @@ from .jellyfish_gen import (
     process_sequence,
     parse_fasta_gz,
     gen_kmer_files,
+    build_kmer_count_table,
 )
 
 __all__ = [
     "process_sequence",
     "parse_fasta_gz",
     "gen_kmer_files",
+    "build_kmer_count_table",
 ]
