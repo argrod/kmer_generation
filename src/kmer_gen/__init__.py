c0258878ff60f05486df5b0085b86c2aa6ad40d6
Aran Garrod <aran.garrod@gmail.com>
2026-07-29 11:11:33 +0100

    Importing complete kmer template function

--- src/kmer_gen/__init__.py
+++ src/kmer_gen/__init__.py
@@ -2,6 +2,7 @@ from .jellyfish_gen import (
     process_sequence,
     parse_fasta_gz,
     gen_kmer_files,
+    create_complete_kmer_template,
     build_kmer_count_table,
 )
 from .kmer_count import calc_kmer
@@ -11,5 +12,6 @@ __all__ = [
     "parse_fasta_gz",
     "gen_kmer_files",
     "build_kmer_count_table",
+    "create_complete_kmer_template",
     "calc_kmer",
 ]
